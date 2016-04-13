f23db82db88c27e6655414399be88d23b11f8a82
Christine <cszc@users.noreply.github.com>
2016-04-13 12:32:14 -0500

    Delete evaluate.py
    evaluate is included in build

--- HW2/evaluate.py
+++
@@ -1,8 +0,0 @@
-'''
-6. Evaluate Classifier: you can use any metric you choose for this assignment
-(accuracy is the easiest one)
-'''
-scores = cross_validation.cross_val_score(
-...    clf, iris.data, iris.target, cv=5)
-
-cross_validation.cross_val_score(model, X, y, scoring='accuracy')
